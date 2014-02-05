01a59b0c2e7771d01676747d39f1421ea03182bd
Mateus R. Ocana <mateus@ocanadesign.com.br>
2014-02-05 17:12:43 -0200

    Sexta versão

---
+++ footer.php
@@ -0,0 +1,33 @@
+      <footer>
+         <small class="copy">&copy; Copyright -
+            <script type="text/javascript">
+                var date = new Date();
+                document.write(date.getFullYear());
+            </script> | Todos Direitos Reservados a <a href="http://flecksys.com.br/" target="_blank" title="Desenvolvido por Flecksys e Ocañadesign.">Flecksys & Ocañadesign.</a>
+         </small>
+      </footer><!--/footer-->
+
+      <!-- javascript -->
+      <script src="_js/jquery.js"></script>
+      <script src="_js/scripts.js"></script>
+      <script src="_js/cycle.js"></script>
+      <script src="_js/cycle_function.js"></script>
+
+      <div id="fb-root"></div>
+      <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=159788717565753";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- google analytics -->
+      <!--<script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+      </script> -->
+   </body>
+</html>
--- index.php
+++ index.php
@@ -1,101 +1,4 @@
-<!doctype html>
-
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="pt-br"> <![endif]-->
-<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="pt-br"> <![endif]-->
-<!--[if IE 8]> <html class="no-js lt-ie9" lang="pt-br"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="pt-br"> <!--<![endif]-->
-
-<head>
-   <meta charset="UTF-8">
-
-   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
-
-   <title>IEQ - Igreja do Evangelho Quadrangular</title>
-
-   <!-- metas tags -->
-   <meta name="keywords" content="your, tags"/>
-   <meta name="description" content="150 words"/>
-   <meta name="copyright"content="company name">
-   <meta name="author" content="name, email@yourdomain.com">
-   <meta name="robots" content="index,follow"/>
-
-   <!-- metas tags open graph protocol -->
-   <meta property="og:site_name" content="IEQ - Igreja do Evangelho Quadrangular"/>
-   <meta property="og:type" content="website"/>
-   <meta property="fb:admins" content=""/>
-   <meta property="og:title" content="IEQ - Igreja do Evangelho Quadrangular"/>
-   <meta property="og:image" content="avatar.png"/>
-   <meta property="og:description" content="Site Descrition"/>
-   <meta name="og:email" content="email@yourdomain.com"/>
-   <meta name="og:phone_number" content="(XX) XXXX-XXXX"/>
-
-   <!-- mobile viewport-->
-   <meta name="viewport" content="width=device-width,initial-scale=1">
-
-   <!-- styles -->
-   <link rel="stylesheet" href="_css/style.css" type="text/css" media="screen">
-   <link rel="stylesheet" href="_css/form-structure.css" type="text/css" media="screen">
-   <link rel="stylesheet" href="_css/form-style.css" type="text/css" media="screen">
-
-   <!-- favicon -->
-   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-
-   <!-- modernizr -->
-   <script src="_js/modernizr.js"></script>
-</head>
-   <body>
-      <!--[if IE 9]><p class="chromeframe">Atenção! seu navegador é <em>muito antigo.</em> <a title="Atualizar meu navegador" href="http://browsehappy.com/">Atualize para uma versão recente</a> ou <a title="Instalar o Chrome Frame" href="http://www.google.com/chromeframe/?redirect=true&prefersystemlevel=true&hl=pt-BR">Instale o Google Chrome Frame</a> para aproveitar todos os recursos deste site.</p><![endif]-->
-
-      <address>
-         <p><b class="fone">Fone:</b> (00) 0000-0000<b class="endereco">Endereço:</b> Rua Engenheiro Annes Gualberto, 1886 - Centro - Tubarão/SC
-
-            <a href="#" title="Clique para fazer login" class="login">Fazer login</a>
-         </p>
-      </address>
-
-   <!-- header -->
-   <header>
-      <div class="topo">
-         <a href="index.php" title="IEQ - Igreja do Evangelho Quadrangular"><img src="_images/logo-ieq.png" alt="Logo IEQ" title="Logo IEQ" class="logo-ieq"></a>
-
-      <div class="localidade">
-         <form action="#">
-            <select name="cidade" id="cidade" placeholder=">Selecione a cidade:">
-               <option value="">São Leopoldo</option>
-               <option value="">Porto Alegre</option>
-               <option value="">Canoas</option>
-               <option value="">Esteio</option>
-               <option value="">Sapucaia</option>
-               <option value="">Monte Negro</option>
-            </select>
-
-            <select name="cidade" id="igreja" placeholder="Selecione a Igreja:">
-               <option value="">Sede</option>
-               <option value="">Filial -1</option>
-               <option value="">Filial -2</option>
-               <option value="">Filial -3</option>
-               <option value="">Filial -4</option>
-               <option value="">Filial -5</option>
-               <option value="">Filial -6</option>
-            </select>
-            <input type="submit" class="localidade-btn" value="Ok" title="Entrar">
-         </form>
-      </div><!-- /form -->
-
-      <img src="_images/logo-bispo.png" alt="Logo IEQ Bispo Sidnei de Mello" class="logo-bispo">
-      </div>
-
-         <nav>
-            <ul>
-               <li><a class="home" title="Home" href="#">Home</a></li>
-               <li><a title="Igrejas" href="#">Igrejas</a></li>
-               <li><a title="Eventos" href="#">Eventos</a></li>
-               <li><a class="galeria" title="Galeria" href="#">Galeria</a></li>
-               <li><a title="Cultos" href="#">Cultos</a></li>
-               <li><a class="atendimento" title="Atendimento" href="#">Atendimento</a></li>
-            </ul>
-         </nav>
-   </header><!-- /header -->
+<?php include "header.php";?>
 
    <!-- slide -->
    <div class="home-slide">
@@ -137,7 +40,7 @@
                <li id="bug"><img src="_images/slide/banner.jpg" title="slide 1" alt=""></li>
          </section>
 
-         <button class="right both">
+         <button class="right">
             <a href="#" title="Ver Todas desta categoria">Ver Todas</a>
          </button>
 
@@ -193,47 +96,33 @@
                </div>
          </div>
 
-         <h1 class="title">Congregações</h1>
+         <div class="congregacoes">
+            <h1 class="title">Congregações</h1>
+               <ul>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt=""></li>
+                  <li><img src="_images/slide/banner.jpg" alt="">
+
+            <button class="right bt">
+               <a href="#" title="Ver Todas desta categoria">Ver Todas</a>
+            </button>
 
 
+                  </li>
+              </ul>
+         </div>
+         </section>
 
-
-
-
-
-
-   </section>
-
-      <footer>
-         <!-- <small class="copy">&copy; Copyright -
-            <script type="text/javascript">
-                var date = new Date();
-                document.write(date.getFullYear());
-            </script> | Todos Direitos Reservados a <a href="http://flecksys.com.br/" target="_blank" title="Desenvolvido por Flecksys e Ocañadesign.">Flecksys & Ocañadesign.</a>
-         </small> -->
-      </footer><!--/footer-->
-
-      <!-- javascript -->
-      <script src="_js/jquery.js"></script>
-      <script src="_js/scripts.js"></script>
-      <script src="_js/cycle.js"></script>
-      <script src="_js/cycle_function.js"></script>
-
-      <div id="fb-root"></div>
-      <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=159788717565753";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- google analytics -->
-      <!--<script>
-        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g,s)}(document,'script'));
-      </script> -->
-   </body>
-</html>
+<?php include "footer.php";?>
