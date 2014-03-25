ecbefe3fa73713fd9bf2929b670ec4c2eb3d54c3
Mateus R. Ocana <mateus@ocanadesign.com.br>
2014-03-25 10:39:19 -0300

    Terminado front-end #4

--- central/index.php
+++ central/index.php
@@ -19,17 +19,90 @@
    </div><!-- /home-slide -->
 
    <section>
+         <figure class="foto-bispo">
+            <img title="Bispo Sidnei de Mello" alt="Foto Bispo Sidnei de Mello" src="_images/thumb-bispo.jpg">
 
-      <h1 class="title left">Eventos da Região</h1>
+            <figcaption>
+               <h1>Sidnei de Mello</h1>
+               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet sed diam nonummy nibh euismod sed diam nonummy.</p>
+               <b>INFORMAÇÕES - REF. IEQ</b>
+            </figcaption>
 
-      <h1 class="title right">(Bom dia) <a title="Visitante" class="fone" href="#">Visitante</a></h1>
+         </figure><!-- foto-bispo -->
+
+         <div class="video-ou-texto">
+            <h1>Titulo da Postagem</h1>
+               <p>
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, eos, assumenda, soluta nihil sequi animi beatae magni adipisci perferendis temporibus doloremque vero reiciendis esse atque est facilis corporis omnis nam.
+                 Ab, doloribus, alias, enim omnis atque ipsam aliquid in blanditiis harum quisquam neque quos distinctio vel facere at sed impedit nam dolores est numquam voluptates sit deserunt aspernatur tenetur nostrum?
+                 Vero, vel unde iure placeat maiores at in facilis iste architecto debitis incidunt necessitatibus excepturi tempore ducimus ex sequi dolores ut inventore? Dolorum, fuga perspiciatis impedit pariatur blanditiis excepturi asperiores.
+                 Dignissimos, odit, fuga expedita cumque dolorum necessitatibus nihil id ratione ipsam a quae sunt fugit esse iste minima dolorem atque nemo suscipit autem rerum magni illum vero error sit doloremque.
+                 Quo, dolorem, perspiciatis, sequi ab cum tempora velit nesciunt quidem praesentium eaque hic ipsum quis fuga vero quaerat quam nisi vitae incidunt maxime ad similique inventore ea fugit quia ducimus.
+                 Ipsa, nihil, rem, aliquid dolor placeat harum enim esse assumenda quasi quia similique quisquam quis iusto quidem illo impedit minima iste ab labore ipsam asperiores autem qui cupiditate quod exercitationem?
+                 Ullam ipsum voluptatibus adipisci minima minus accusantium expedita at perspiciatis. Dicta, consequuntur, aut fuga veritatis a quos sapiente beatae voluptatem repudiandae deleniti eum officia harum. Cumque numquam facere
+                  ipsam asperiores autem qui cupiditate quod exercitationem?
+                 Ullam ipsum voluptatibus adipisci minima minus accusantium expedita at perspiciatis. Dicta, consequuntur, aut fuga veritatis a quos sapiente beatae voluptatem repudiandae deleniti eum officia harum. Cumque numquam facere
+               </p>
+
+               <!-- <iframe src="//www.youtube.com/embed/_9IdF-MN3aY" frameborder="0" allowfullscreen></iframe> -->
+                <!-- se cadastrar um video no painel irá aparecer preenchendo toda box -->
+         </div><!-- video-ou-texto -->
+
+            <div class="fotos-ou-slide">
+
+               <div class="slide">
+                  <ul>
+                     <li><img src="_images/slide/banner3.jpg" title="slide 1" alt=""></li>
+                     <li><img src="_images/slide/banner2.jpg" title="slide 2" alt=""></li>
+                     <li><img src="_images/slide/banner.jpg" title="slide 3" alt=""></li>
+                     <li><img src="_images/slide/banner4.jpg" title="slide 4" alt=""></li>
+                     <li><img src="_images/slide/banner5.jpg" title="slide 5" alt=""></li>
+                  </ul>
+               </div>
+            </div><!-- fotos-ou-slide -->
+
+            <div class="clear"></div>
+         <div class="noticias-text">
+
+<!--          <a class="prev" title="Anterior" href="#">Anterior</a>
+         <a class="next" title="Próximo" href="#">Próximo</a> -->
+
+            <h1>Titulo da Postagem</h1>
+               <p>
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, praesentium, nemo, sint odio commodi suscipit neque dolore nisi voluptates modi eligendi itaque porro consequuntur sed fugit. Voluptas, provident neque quod.
+                 Eum, hic tempora nemo tenetur nulla neque similique harum facilis magnam! Non, eaque quibusdam deserunt quisquam quos atque autem recusandae nisi quia ullam? Fugit, iure nemo consectetur repellendus nesciunt laudantium?
+                 Debitis, velit ad ipsa. Eaque, incidunt iusto velit hic iste laborum totam natus ut ducimus commodi?
+               </p>
+         </div><!-- noticias-text -->
+
+         <div class="video-text">
+      <!-- <h1>Titulo da Postagem</h1>
+               <p>
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, praesentium, nemo, sint odio commodi suscipit neque dolore nisi voluptates modi eligendi itaque porro consequuntur sed fugit. Voluptas, provident neque quod.
+                 Eum, hic tempora nemo tenetur nulla neque similique harum facilis magnam! Non, eaque quibusdam deserunt quisquam quos atque autem recusandae nisi quia ullam? Fugit, iure nemo consectetur repellendus nesciunt laudantium?
+                 Debitis, velit ad ipsa. Eaque, incidunt iusto velit hic iste laborum totam natus ut ducimus commodi?
+               </p> -->
+               <iframe src="//www.youtube.com/embed/_9IdF-MN3aY" frameborder="0" allowfullscreen></iframe>
+         </div><!-- video-text -->
+
+         <div class="question">
+               <span>
+                  Oquê você achou do nosso portal? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, aliquid, rerum, vel, corporis ea commodi maxime blanditiis reprehenderit.
+               </span>
+            <button class="btn red" type="submit">Responder</button>
+         </div><!-- question -->
+
+         <div class="acess">
+            <h1>Total de acessos no site</h1>
+            <p>1850</p>
+         </div><!-- acess -->
    </section>
 
 <?php include "site/footer.php";?>
 
-<!-- remove a classe nav - menu -->
+<!-- remove class -->
 <script type="text/javascript">
    $(function(){
-      $('nav').remove();
+      $('nav, .telephone, .f-none').remove();
    });
 </script>
